c2b64e6210864fdc6c7ead2b18e5272f45e7018a
Tharmes <tharmeselvam@gmail.com>
2025-10-01 14:59:46 +0800

    Implement create tweet function

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Request, Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { TweetsService } from './tweets.service';
+import { CreateTweetDto } from './dto/create-tweet.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('tweets')
-export class TweetsController {}
+export class TweetsController {
+    constructor (
+        private tweetsService: TweetsService
+    ){}
+
+    @UseGuards(AuthGuard)
+    @Post('create')
+    async createTweet (@Body() payload: CreateTweetDto, @Request() request){
+        const userId = request.payload.sub;
+        return await this.tweetsService.createTweet(payload, userId);
+    }
+
+}
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tweet } from 'src/entities/tweet.entity';
+import { Repository } from 'typeorm';
+import { CreateTweetDto } from './dto/create-tweet.dto';
 
 @Injectable()
-export class TweetsService {}
+export class TweetsService {
+    constructor (
+        @InjectRepository(Tweet)
+        private tweetsRepository: Repository<Tweet>
+    ){}
+
+    async createTweet (payload: CreateTweetDto, userId: number, ){
+        const tweet =  await this.tweetsRepository.create({
+            content: payload.content,
+            user: { id: userId }
+        });
+
+        return await this.tweetsRepository.save(tweet);
+    }
+}
