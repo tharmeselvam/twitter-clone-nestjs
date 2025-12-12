96cc0952489249cb5a3076540efd3a7d4fb96e15
Tharmes <tharmeselvam@gmail.com>
2025-12-12 16:17:08 +0800

    Make search endpoints guarded

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, DefaultValuePipe, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { Controller, DefaultValuePipe, Get, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Controller('search')
 export class SearchController {
@@ -7,6 +8,7 @@ export class SearchController {
         private searchService: SearchService
     ){}
 
+    @UseGuards(AuthGuard)
     @Get('users')
     async searchUsers(
         @Query('search') search: string,
@@ -15,6 +17,7 @@ export class SearchController {
         return await this.searchService.searchUsers(search, page);
     }
 
+    @UseGuards(AuthGuard)
     @Get('tweets')
     async searchTweets(
         @Query('search') search: string,
